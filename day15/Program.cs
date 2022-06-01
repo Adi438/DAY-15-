332d3f99e57a05311adc45b89324eb72226683f2
Adi438 <mishraaaditya54@gmail.com>
2022-06-01 22:27:25 +0530

    uc1 code

--- day15/Program.cs
+++ day15/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace day15
+namespace BSTAndHashTable
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello, welcome to HashTable Problem");
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value is: " + hash5);
         }
     }
 }
